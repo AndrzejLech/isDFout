e937b6c6129026578050fdd7cf3c7c476cd81d88
Andrzej Lech <andrzejdlech@gmail.com>
2021-10-11 23:21:19 +0200

    add /get

--- isdwarffortressoutyet.go
+++ isdwarffortressoutyet.go
@@ -12,6 +12,7 @@ func main() {
 	router.LoadHTMLGlob("view.html")
 
 	router.GET("/", getInfo)
+	router.GET("/get")
 	router.Run()
 
 }
@@ -40,4 +41,30 @@ func getInfo(context *gin.Context) {
 	}
 
 	context.HTML(200, "view.html", gin.H{"result": result})
+}
+
+func getJSON(context *gin.Context) {
+	result := ""
+	url := "https://store.steampowered.com/app/975370/Dwarf_Fortress/"
+	c := colly.NewCollector()
+
+	c.OnRequest(func(r *colly.Request) {
+		fmt.Println("Visiting", r.URL)
+	})
+
+	c.OnHTML("div.content", func(div *colly.HTMLElement) {
+		div.ForEach("span", func(index int, element *colly.HTMLElement) {
+			if index == 1 {
+				result = element.Text
+			}
+		})
+	})
+
+	c.Visit(url)
+
+	if result != "time is subjective" {
+		result = "yes"
+	}
+
+	context.JSON(200, result)
 }
